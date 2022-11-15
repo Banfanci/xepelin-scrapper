50662f603e461cccec57513d7648b961acce916b
banfanci <bflobos@uc.cl>
2022-11-15 13:02:28 -0300

    fix for not have a timeout

--- index.js
+++ index.js
@@ -14,14 +14,15 @@ const googleSheetId = "1B66yG75efaAixmUB6Gtu_nCNUmyujDbpaD4PoEb8SdA";
 
 app.post("/getArticlesOfCategory", async (req, res) => {
   const { category, webhook } = req.body;
+  res.status(200).json();
   const articles = await scrapper.getAllArticlesOfOneSection(category);
   await googleSheet.addSheetWithArticles(category, articles, googleSheetId);
   await webhookHandler.sendWebhookMessage(webhook, googleSheetId);
-  res.status(200).json({ message: `${category} scrapped succesfully` });
 });
 
 app.post("/getAllArticles", async (req, res) => {
   const { webhook } = req.body;
+  res.status(200).json();
   const articles = await scrapper.getAllArticlesOfAllSections();
   await googleSheet.addSheetWithArticles(
     "All Articles",
@@ -29,7 +30,6 @@ app.post("/getAllArticles", async (req, res) => {
     googleSheetId
   );
   await webhookHandler.sendWebhookMessage(webhook, googleSheetId);
-  res.status(200).json({ message: "all categories scrapped succesfully" });
 });
 
 const port = process.env.PORT || 3000;
